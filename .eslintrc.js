39e9686f317165b6aaa57c3089b03ce0986a37c1
RaymondAfan <raymond.afan@icloud.com>
2024-02-09 15:59:50 -0800

    lint stuff

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,23 +4,15 @@ module.exports = {
     commonjs: true,
     es2021: true,
   },
-  extends: 'airbnb-base',
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: [
-        '.eslintrc.{js,cjs}',
-      ],
-      parserOptions: {
-        sourceType: 'script',
-      },
-    },
-  ],
+  // update the extensions
+  extends: ["eslint:recommended", "airbnb-base", "prettier"],
+  overrides: [],
   parserOptions: {
-    ecmaVersion: 'latest',
+    ecmaVersion: "latest",
   },
   rules: {
+    "no-console": "off",
+    "no-underscore-dangle": ["error", { "allow": ["_id"] }],
+    //make it an error to use undersocres unless identifier is foo_ or _bar
   },
 };
