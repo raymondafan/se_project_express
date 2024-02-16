5c90ceb8b41457936cbce4b87fda20097c31ff5d
RaymondAfan <raymond.afan@icloud.com>
2024-02-15 17:41:05 -0800

    imageUrl

--- controllers/clothingItem.js
+++ controllers/clothingItem.js
@@ -3,9 +3,9 @@ const ClothingItem = require("../models/clothingItem");
 const createItem = (req, res) => {
   console.log(req);
   console.log(req.body);
-  const { name, weather, imageURL } = req.body;
+  const { name, weather, imageUrl } = req.body;
 
-  ClothingItem.create({ name, weather, imageURL })
+  ClothingItem.create({ name, weather, imageUrl })
 
     .then((item) => {
       console.log(item);
@@ -34,9 +34,9 @@ const getItems = (req, res) => {
 const updateItem = (req, res) => {
   const { itemId } = req.params;
 
-  const { imageURL } = req.body;
+  const { imageUrl } = req.body;
 
-  ClothingItem.findByIdAndUpdate(itemId, { $set: { imageURL } })
+  ClothingItem.findByIdAndUpdate(itemId, { $set: { imageUrl } })
     .orFail()
     .then((item) => {
       res.status(200).send({ data: item });
