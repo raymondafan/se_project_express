584069750f7107a33aa719f5cb8f31a7fd2112fe
RaymondAfan <raymond.afan@icloud.com>
2024-02-26 19:55:07 -0800

    added routes and controllers for sign up/sign in

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,12 @@ const router = require("express").Router();
 const clothingItem = require("./clothingItem");
 const userRouter = require("./users");
 const { NOT_FOUND } = require("../utils/errors");
+const { createUser, login } = require("../controllers/users");
 
 router.use("/items", clothingItem); // baseUrl/items
 router.use("/users", userRouter);
+router.post("/signin", login);
+router.post("/signup", createUser);
 
 router.use((req, res) => {
   res.status(NOT_FOUND).send({ message: "Router not found" });
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
-const { getUsers, createUser, getUser } = require("../controllers/users");
+const { getUsers, createUser, getUser, login } = require("../controllers/users");
 
-router.get("/", getUsers);
-router.get("/:userId", getUser);
-router.post("/", createUser);
+// router.get("/", getUsers);
+// router.get("/:userId", getUser);
+// router.post("/", createUser);
+
 module.exports = router;
