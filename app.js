b7da7a1121c18c8aec65bd499cbad3530d152666
RaymondAfan <raymond.afan@icloud.com>
2024-03-06 15:53:29 -0800

    unused code

--- app.js
+++ app.js
@@ -26,12 +26,7 @@ mongoose
 // It is mostly used to set up middleware for your application.
 
 app.use(express.json());
-// app.use((req, res, next) => {
-//   req.user = {
-//     _id: '5d8b8592978f8bd833ca8133'// paste the _id of the test user created in the previous step
-//   };
-//   next();
-// });
+
 
 app.use(mainRouter);
 
