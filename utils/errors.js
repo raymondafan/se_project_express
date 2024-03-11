f2d210fa262fb0be4ac179cb7b6c0938c1753b1b
RaymondAfan <raymond.afan@icloud.com>
2024-03-11 16:26:47 -0700

    deleted comments

--- utils/errors.js
+++ utils/errors.js
@@ -16,26 +16,4 @@ module.exports = {
   REQUEST_CONFLICT,
   FORBIDDEN,
 };
-// const deleteItem = (req, res) => {
-//   const { itemId } = req.params;
-//   console.log(itemId);
-//   ClothingItem.findByIdAndDelete(itemId)
-//     .orFail()
-//     .then((item) => {
-//       if (itemId === user._id) {
-//         res.status(OK).set("Content-Type", "application/json").send(item);
-//       }
-//     })
-//     .catch((err) => {
-//       console.error(err);
-//       if (err.name === "DocumentNotFoundError") {
-//         return res.status(403).send({ message: "Invalid data" });
-//       }
-//       if (err.name === "CastError") {
-//         return res.status(BAD_REQUEST).send({ message: err.message });
-//       }
-//       return res
-//         .status(INTERNAL_SERVER_ERROR)
-//         .send({ message: "An error has occurred on the server." });
-//     });
-// };
+
