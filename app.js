baadbbff24250c7c5e9b62a085852da058ea37ff
RaymondAfan <raymond.afan@icloud.com>
2024-05-14 19:06:21 -0700

    update backend repo

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require("express");
 const errorHandler = require("./utils/errors");
 const app = express();
--- utils/config.js
+++ utils/config.js
@@ -1,4 +1,4 @@
-const JWT_SECRET = "secret-stuff";
+const { JWT_SECRET = "super-strong-secret" } = process.env;
 
 module.exports = {
   JWT_SECRET,
